559ee285614dd723ea26b4a565378b58d10bd2e5
Akhilesh-Yadav6283 <135110548+Akhilesh-Yadav6283@users.noreply.github.com>
2023-11-24 17:14:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+for(let i=0; i<=20; i++){
+	if(i%3==0){
+		alert("Fizz");
+	}
+	if(i%5==0){
+		alert("Buzz");
+	}
+	if(i%3==0 && i%5==0){
+		alert("FizzBuzz");
+	}
+}
